3f4be96a9c8aebb25e00352fec52c6965bc169c0
Thelnfamous1 <junior.garay1999@gmail.com>
2023-11-14 02:05:33 -0600

    Switched from shadow to jarjar. Used flatdir dependency format for adventur platform and modified boosted yaml. Still having issues launching due to ResolutionException for module duplication, if the adventure api dependency is removed, the game launchs but crashes upon world selection/creation. Also fixed mod initialization.

--- src/main/java/me/Thelnfamous1/huskhomes/HuskHomesMod.java
+++ src/main/java/me/Thelnfamous1/huskhomes/HuskHomesMod.java
@@ -6,7 +6,10 @@ import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.william278.huskhomes.ForgeHuskHomes;
 import org.slf4j.Logger;
 
@@ -19,15 +22,20 @@ public class HuskHomesMod {
     public HuskHomesMod() {
         this.huskHomes = new ForgeHuskHomes();
 
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::onCommonSetup);
         MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
         MinecraftForge.EVENT_BUS.addListener(this::onServerAboutToStart);
         MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
         MinecraftForge.EVENT_BUS.addListener(this::onServerStopping);
     }
 
+    private void onCommonSetup(FMLCommonSetupEvent event){
+        event.enqueueWork(this.huskHomes::onInitializeServer);
+    }
+
     private void onServerAboutToStart(ServerAboutToStartEvent event){
         this.huskHomes.setMinecraftServer(event.getServer());
-        this.huskHomes.onInitializeServer();
     }
 
     private void onRegisterCommands(RegisterCommandsEvent event) {
